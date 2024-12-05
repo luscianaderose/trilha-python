df56ee914e841d030f9865ad2d37bfd7119b58dc
luscianaderose <luscianaderose@gmail.com>
2024-12-05 19:29:33 -0300

    q06

--- casa/semana03/q06.py
+++ casa/semana03/q06.py
@@ -1,15 +1,22 @@
 # Exercício 06: Gerenciamento de Estoque de uma Loja de Alimentos - Atualizar um Produto
-# Continuando o exercício anterior, onde você implementou a funcionalidade de remover produtos do estoque, agora o sistema deve permitir que o usuário atualize as
+
+# Continuando o exercício anterior, onde você implementou a funcionalidade de remover 
+# produtos do estoque, agora o sistema deve permitir que o usuário atualize as
 # informações de um produto já existente no estoque.
+
 # Tarefas:
+
 # 1. Atualizar Produto:
-# O sistema deve permitir que o usuário atualize as informações de um produto. O usuário deverá escolher um produto pelo seu identificador e fornecer as
+# O sistema deve permitir que o usuário atualize as informações de um produto. 
+# O usuário deverá escolher um produto pelo seu identificador e fornecer as
 # novas informações para o produto:
 # Novo nome (opcional, caso o nome precise ser alterado).
 # Novo preço (opcional, caso o preço precise ser alterado).
 # Nova quantidade (opcional, caso a quantidade precise ser alterada).
+
 # 2. Exibição da Lista de Produtos Após Atualização:
-# Após atualizar as informações do produto, o sistema deve exibir a lista atualizada de produtos, com as mudanças feitas.
+# Após atualizar as informações do produto, o sistema deve exibir a lista atualizada de produtos, 
+# com as mudanças feitas.
 # Exemplo de Saída:
 # Produtos disponíveis:
 # 1 - Arroz: R$ 7.49 (10 unidades)
@@ -17,11 +24,14 @@
 # 3 - Ovos: R$ 0.70 (200 unidades)
 # 4 - Macarrão: R$ 3.50 (50 unidades)
 # 5 - Açúcar: R$ 2.30 (30 unidades)
+
 # Digite o identificador do produto que deseja atualizar: 2
 # Digite o novo nome (ou pressione Enter para manter o nome atual): Feijão Preto
 # Digite o novo preço (ou pressione Enter para manter o preço atual): 10.99
 # Digite a nova quantidade (ou pressione Enter para manter a quantidade atual): 25
+
 # Produto atualizado com sucesso.
+
 # Produtos disponíveis:
 # 1 - Arroz: R$ 7.49 (10 unidades)
 # 2 - Feijão Preto: R$ 10.99 (25 unidades)
@@ -59,9 +69,21 @@ def exibir_estoque():
 
 exibir_estoque()
 
-remover = int(input('\nDigite o identificador do produto que deseja remover: ')) - 1
-produto_removido = list(produtos.keys())[remover] # PEGANDO O NOME DO PRODUTO RELATIVO AO IDENTIFICADOR
-produtos.pop(produto_removido)
-print(f'Produto {produto_removido.capitalize()} removido do estoque.')
+id_alterar = int(input('\nDigite o identificador do produto que deseja alterar: ')) - 1
+nome_original = list(produtos.keys())[id_alterar]
+novo_nome = input('Digite o novo nome (ou pressione Enter para manter o nome atual): ') or nome_original
+novo_preco = input('Digite o novo preço (ou pressione Enter para manter o preço atual): ').strip()
+nova_qtd = input('Digite a nova quantidade (ou pressione Enter para manter a quantidade atual): ').strip()
+
+if novo_nome != nome_original:
+    produtos[novo_nome] = produtos.pop(nome_original)
+
+if novo_preco:
+    produtos[novo_nome]['preço'] = float(novo_preco)
+
+if nova_qtd:
+    produtos[novo_nome]['quantidade'] = int(nova_qtd)
+
+print(f'Produto {novo_nome.capitalize()} alterado com sucesso.')
 
 exibir_estoque()
